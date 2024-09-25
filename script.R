1fc867c2e149a60fb0de86da26ac5c68516a9c14
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-09-25 13:17:46 +0200

    rm debug prints

--- script.R
+++ script.R
@@ -63,10 +63,6 @@ setwd(normalizePath(file.path(args[1])))
 number_of_workers <- as.integer(args[2])
 timeout <- as.integer(args[3])
 
-# debug print - @TODO: remove
-print(.libPaths())
-print(installed.packages()[1:3, c("Package", "LibPath", "Version")])
-
 # Install required packages
 catnl("Installing required packages...")
 if (!requireNamespace("pak", quietly = TRUE)) {
